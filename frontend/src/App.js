85f66a0fa7f2bb52c49df33dd03ba4bfbe669e97
SaiCarthikeya <saicarthik180@gmail.com>
2024-09-21 20:28:42 +0530

    initial-setup-with-tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p className='text-green-500 text-8xl'>test</p>
+      <h1 className="text-3xl text-red-900 bg-slate-600 font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
